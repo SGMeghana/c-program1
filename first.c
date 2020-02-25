5f78b737d3e59bd138efc2fd6f19a843a379f337
SGMeghana <61412838+SGMeghana@users.noreply.github.com>
2020-02-25 12:42:04 +0530

    Add files via upload
    sample c program

---
+++ first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,r,c;
+printf("radius of circle");
+scanf("%d",&r);
+a=3.14*r*r;
+c=2*3.14*r;
+printf("the area of circle is %d\n the circumference of circle is %d\n",a,c);
+}
